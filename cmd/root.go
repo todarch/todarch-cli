d54c289a412afb83808abe30d6044b8f1f5ef6dd
selimssevgi <selimssevgi@gmail.com>
2018-09-28 21:56:43 +0300

    use viper for global configuration
    - add debug and verbose flags

--- cmd/root.go
+++ cmd/root.go
@@ -3,16 +3,25 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"github.com/todarch/todarch-cli/consts"
 	"os"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "todarch",
-	Short: "Todarch is a todo archieve manager",
+	Short: "Todarch is a todo achieve manager",
 	Long:  "Collect your todos, and get them done whenever you want.",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Root command")
 	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP(consts.VERBOSE, "v", false, "verbose output")
+	rootCmd.PersistentFlags().BoolP(consts.DEBUG, "d", false, "debug application")
+	viper.BindPFlag(consts.VERBOSE, rootCmd.PersistentFlags().Lookup(consts.VERBOSE))
+	viper.BindPFlag(consts.DEBUG, rootCmd.PersistentFlags().Lookup(consts.DEBUG))
 }
 
 func Execute() {
--- util/mlog.go
+++ util/mlog.go
@@ -1,17 +1,19 @@
 package util
 
 import (
+	"github.com/spf13/viper"
+	"github.com/todarch/todarch-cli/consts"
 	"log"
-	"os"
 )
 
 func Log(msg interface{}) {
-	if isDebugEnabled() {
+	if viper.GetBool(consts.VERBOSE) {
 		log.Println(msg)
 	}
 }
 
-func isDebugEnabled() bool {
-	debug := os.Getenv("TODARCH_DEBUG")
-	return debug != ""
+func Debug(msg interface{}) {
+	if viper.GetBool(consts.DEBUG) {
+		log.Println(msg)
+	}
 }
